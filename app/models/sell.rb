94a80d4b2728a53e8d4986ca0c5d0804e572f389
Rodrigo Rosa <rodrigo@wonder.com.br>
2017-11-04 10:42:36 -0200

    Adicionados models para itens produtos e serviços

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -3,6 +3,12 @@ class Sell < ApplicationRecord
   enum status: { finished: 0, canceled: 1 }
   validates :client, presence: true
 
+  has_many :sell_products
+  has_many :products, through: :sell_products
+
+  has_many :sell_services
+  has_many :services, through: :sell_services
+
   def fae_display_field
     id
   end
